abe449614c3f0398d0abb287163b51c3569c58fe
Tiep Phan <tieppt12@gmail.com>
2020-06-03 22:30:43 +0700

    add SEO info for home page

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from '../../services/seo.service';
+import { environment } from '@tp-blog/env/environment';
 
 @Component({
   selector: 'tp-home',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seoService: SeoService) { }
 
   ngOnInit(): void {
+    this.seoService.setData({
+      author: 'Tiep Phan',
+      title: 'Tiep Phan | Lập Trình Thật Kỳ Diệu',
+      url: environment.siteUrl,
+      description: 'Welcome to my blog. I\'m Tiep Phan, a Google Developer Expert in Angular.'
+    });
   }
 
 }
