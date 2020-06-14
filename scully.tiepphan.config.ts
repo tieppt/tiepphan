f7da2f50314a80d6016d9279ba12c781e59c116b
Tiep Phan <tieppt12@gmail.com>
2020-06-14 13:03:49 +0700

    feat(infra): support sitemap

--- scully.tiepphan.config.ts
+++ scully.tiepphan.config.ts
@@ -1,9 +1,19 @@
 import { ScullyConfig, RouteTypes, setPluginConfig } from '@scullyio/scully';
 import { HljsHtml } from './plugins/hljs';
-import { TocConfig, getTocPlugin} from 'scully-plugin-toc';
+import { TocConfig, getTocPlugin } from 'scully-plugin-toc';
 import { getHttp404Plugin } from '@gammastream/scully-plugin-http404';
+import { getSitemapPlugin } from '@gammastream/scully-plugin-sitemap';
 
 const Http404Plugin = getHttp404Plugin();
+
+const SitemapPlugin = getSitemapPlugin();
+setPluginConfig(SitemapPlugin, {
+  urlPrefix: 'https://www.tiepphan.com',
+  sitemapFilename: 'sitemap.xml',
+  changeFreq: 'monthly',
+  priority: ['1.0', '0.9', '0.8', '0.7', '0.6', '0.5', '0.4', '0.3', '0.2', '0.1', '0.0'],
+  ignoredRoutes: ['/404'],
+});
 
 const tocOptions: TocConfig = {
   blogAreaSelector: '.blog-content',
